30ca99a4fa4c605825ffc9026efdbeaee398d4e4
Daniel <daniel.kaska2004@gmail.com>
2024-01-27 13:18:31 +0100

    add account controller

--- Forum/Controllers/AccountController.cs
+++ Forum/Controllers/AccountController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Forum.DB;
+using Forum.Models;
+using Forum.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Forum.Controllers
 {
     [ApiController]
     public class AccountController : Controller
     {
-        public ActionResult CreateAccount()
+        /*---------------dependencies----------------*/
+        private readonly UserService userService;
+        private readonly ForumDbContext db;
+        private readonly IMapper mapper;
+        /*-------------------------------------------*/
+
+        public AccountController(UserService userService, ForumDbContext _db, IMapper _mapper)
         {
+            this.userService = userService;
+            db = _db;
+            mapper = _mapper;
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateAccount(UserModel user)
+        {
+            if (user is null)
+                return BadRequest("user is null");
+
+
+
 
             return BadRequest();
         }
