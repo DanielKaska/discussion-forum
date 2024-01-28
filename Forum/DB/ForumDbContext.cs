b6b8feca9db57df669fe188074ba6549590372f9
Daniel <daniel.kaska2004@gmail.com>
2024-01-28 18:03:53 +0100

    changed entities properties

--- Forum/DB/ForumDbContext.cs
+++ Forum/DB/ForumDbContext.cs
@@ -5,7 +5,7 @@ namespace Forum.DB
 {
     public class ForumDbContext : DbContext 
     {
-        private string connectionString = "Server=localhost;Database=ForumDB;Integrated Security=True;TrustServerCertificate=True;";
+        private readonly string connectionString = "Data Source=DANIEL;Initial Catalog=ForumDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
@@ -13,8 +13,16 @@ namespace Forum.DB
         protected override void OnModelCreating(ModelBuilder mb)
         {
             mb.Entity<User>().Property(u => u.Id).IsRequired();
-            mb.Entity<User>().Property(u => u.Email).IsRequired();
-            mb.Entity<User>().Property(u => u.Password).IsRequired();
+            mb.Entity<User>().Property(u => u.Email).IsRequired().HasMaxLength(40);
+            mb.Entity<User>().Property(u => u.Password).IsRequired().HasMaxLength(30);
+            mb.Entity<User>().Property(u => u.Name).IsRequired().HasMaxLength(20);
+            mb.Entity<User>().Property(u => u.RoleId).HasDefaultValue(1);
+                
+
+            mb.Entity<Role>().HasData(new Role() { Id = 1, Name = "User", Description = "Normal user, can create posts and comment"});
+            mb.Entity<Role>().HasData(new Role() { Id = 2, Name = "Moderator", Description = "Moderator, can mute or ban users, and delete their posts" });
+            mb.Entity<Role>().HasData(new Role() { Id = 3, Name = "Admin", Description = "Admin, has right to do everything" });
+
         }
 
 
