56cb09a49b0386aec53d6ffc71740a33407b966f
Daniel <daniel.kaska2004@gmail.com>
2024-01-29 12:46:24 +0100

    account creating logic moved from controller to service, added base for logging in

--- Forum/Services/UserService.cs
+++ Forum/Services/UserService.cs
@@ -1,10 +1,49 @@
-﻿namespace Forum.Services
+﻿using AutoMapper;
+using Forum.DB;
+using Forum.DB.Entities;
+using Forum.Models;
+using Isopoh.Cryptography.Argon2;
+
+namespace Forum.Services
 {
     public class UserService
     {
-        public void CreateUser()
-        {
 
+        /*---------------dependencies----------------*/
+        private readonly ForumDbContext db; //Db Context
+        private readonly IMapper mapper; //AutoMapper
+        /*-------------------------------------------*/
+
+        public UserService(ForumDbContext _db, IMapper _mapper)
+        {
+            db = _db;
+            mapper = _mapper;
         }
+
+        public User CreateUser(UserModel um)
+        {
+            var user = mapper.Map<User>(um);
+            var hashedPassword = Argon2.Hash(user.Password);
+            user.Password = hashedPassword;
+
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            return user;
+        }
+
+        //todo
+        //login method checks if the user sent correct email and password, then sends back jwt token
+        public bool Login(UserModel um)
+        {
+            var user = db.Users.FirstOrDefault(u => u.Email == um.Email);
+            
+            if (user is null)
+                return false;
+
+
+            return false;
+        }
+
     }
 }
