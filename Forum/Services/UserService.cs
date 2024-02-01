17d30a9bc885f41d1074004bef4fbc11e89b625e
Daniel <daniel.kaska2004@gmail.com>
2024-02-01 11:09:57 +0100

    added jwt authentication

--- Forum/Services/UserService.cs
+++ Forum/Services/UserService.cs
@@ -3,6 +3,12 @@ using Forum.DB;
 using Forum.DB.Entities;
 using Forum.Models;
 using Isopoh.Cryptography.Argon2;
+using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Forum.Services
 {
@@ -12,12 +18,14 @@ namespace Forum.Services
         /*---------------dependencies----------------*/
         private readonly ForumDbContext db; //Db Context
         private readonly IMapper mapper; //AutoMapper
+        private readonly JwtSettings jwtSettings;
         /*-------------------------------------------*/
 
-        public UserService(ForumDbContext _db, IMapper _mapper)
+        public UserService(ForumDbContext _db, IMapper _mapper, JwtSettings _JwtSettings)
         {
             db = _db;
             mapper = _mapper;
+            jwtSettings = _JwtSettings;
         }
 
         public User CreateUser(UserModel um)
@@ -34,15 +42,35 @@ namespace Forum.Services
 
         //todo
         //login method checks if the user sent correct email and password, then sends back jwt token
-        public bool Login(UserModel um)
+        public string Login(UserModel um)
         {
             var user = db.Users.FirstOrDefault(u => u.Email == um.Email);
             
             if (user is null)
-                return false;
+                return null;
+
+            if(Argon2.Verify(user.Password, um.Password)) //if the password is correct, return token
+            {
+
+                List<Claim> claims = new()
+                {
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim("id", user.Id.ToString()),
+                    new Claim("roleId", user.RoleId.ToString())
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                
+                var token = new JwtSecurityToken(jwtSettings.Issuer, jwtSettings.Audience, claims, expires: DateTime.Now.AddDays(30), signingCredentials: creds);
 
 
-            return false;
+                return new JwtSecurityTokenHandler().WriteToken(token);
+
+
+            }
+            return null;
         }
 
     }
