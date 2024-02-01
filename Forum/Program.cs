49f1d2e0e11c08399987a933b3c4eab713a88965
Daniel <daniel.kaska2004@gmail.com>
2024-02-01 11:10:08 +0100

    jwt configuration

--- Forum/Program.cs
+++ Forum/Program.cs
@@ -1,8 +1,43 @@
 using Forum.DB;
 using Forum.Models;
 using Forum.Services;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// --------------------------------------------- JWT ---------------------------------------------  //
+
+//get jwt options from appsettings.json
+var jwtOptions = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+
+//configure authenticaction
+builder.Services.AddAuthentication(
+    //set auth scheme to bearer
+    option =>
+    {
+        option.DefaultAuthenticateScheme = "Bearer";
+        option.DefaultScheme = "Bearer";
+        option.DefaultChallengeScheme = "Bearer";
+    })
+ 
+    .AddJwtBearer(options =>
+    {
+        //set bearer options 
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtOptions.Issuer,
+            ValidAudience = jwtOptions.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Secret))
+        };
+    }
+);
+
+//----------------------------------------------------------------------------------------------------//
 
 // Add services to the container.
 
@@ -11,11 +46,13 @@ builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(MapperConfig));
 builder.Services.AddSingleton<UserService>();
 builder.Services.AddSingleton<ForumDbContext>();
-
+builder.Services.AddSingleton(jwtOptions);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
