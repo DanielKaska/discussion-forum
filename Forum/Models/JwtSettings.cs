e38f9f396c7bf7d58dff1be93051ab5936a6cb64
Daniel <daniel.kaska2004@gmail.com>
2024-02-01 11:09:33 +0100

    JwtSettings class created

---
+++ Forum/Models/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Forum.Models
+{
+    public class JwtSettings
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Secret { get; set; }
+    }
+}
