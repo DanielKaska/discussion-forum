99b6cfb877de59ac0ddb58709be85c51566d37c8
Daniel <daniel.kaska2004@gmail.com>
2024-02-03 15:52:33 +0100

    added post controller with CreatePost, and ModifyPost method

---
+++ Forum/Controllers/PostController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Forum.DB;
+using Forum.Models;
+using Forum.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Controllers
+{
+
+    [ApiController]
+    public class PostController : Controller
+    {
+
+        /*---------------dependencies----------------*/
+        private readonly PostService postService;
+        /*-------------------------------------------*/
+
+        public PostController(PostService postService)
+        {
+            this.postService = postService;
+        }
+
+        [Authorize]
+        [HttpPost("post/create")]
+        public ActionResult CreatePost([FromBody] PostModel pModel)
+        {
+            if (pModel is null)
+                return BadRequest("Post model cant be null");
+
+            var userIdClaim = Request.HttpContext.User.Claims.FirstOrDefault(c => c.Type == "id");
+
+            if(userIdClaim is not null)
+            {
+                var postId = postService.CreatePost(pModel, int.Parse(userIdClaim.Value));
+                return Ok(postId);
+            }
+
+            return BadRequest();
+            
+        }
+
+        [Authorize]
+        [HttpPost("post/modify/{postId}")]
+        public ActionResult ModifyPost([FromRoute] int postId, [FromBody] PostModel pModel)
+        {
+            var userIdClaim = Request.HttpContext.User.Claims.FirstOrDefault(c => c.Type == "id");
+            var userRoleClaim = Request.HttpContext.User.Claims.FirstOrDefault(c => c.Type == "roleId");
+
+            if(userIdClaim is null || userRoleClaim is null)
+                BadRequest("Token error");
+
+            var modified = postService.ModifyPost(postId, pModel, int.Parse(userIdClaim.Value), int.Parse(userRoleClaim.Value));
+
+            if (modified)
+                return Ok();
+            
+            return BadRequest();
+        }
+
+
+    }
+}
