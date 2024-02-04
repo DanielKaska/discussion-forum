061c88dab74ccc661132ca7fabedc2ba45fd86f7
Daniel <daniel.kaska2004@gmail.com>
2024-02-04 19:58:25 +0100

    Added date of creation of the account, and one to many relation (user to posts)

--- Forum/DB/Entities/User.cs
+++ Forum/DB/Entities/User.cs
@@ -8,7 +8,11 @@ namespace Forum.DB.Entities
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public DateTime CreatedDate { get; set; }
 
+        public List<Post> Posts { get; set; }
+
+        public virtual Role Role { get; set; }
         public int RoleId { get; set; }
     }
 }
