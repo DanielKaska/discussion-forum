569cf24d5d1f11d7a55a443446723e3d452dd88e
Daniel <daniel.kaska2004@gmail.com>
2024-02-08 19:21:33 +0100

    added middleware for error handling

---
+++ Forum/ErrorMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+using Serilog;
+
+namespace Forum
+{
+    public class ErrorMiddleware : IMiddleware
+    {
+
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch(Exception ex)
+            {
+                Log.Error(ex.Message);
+                Debug.WriteLine($"Error: {ex.Message}"); 
+            }
+
+        }
+
+    }
+}
