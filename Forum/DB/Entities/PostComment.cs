8010caa19844a7277127a094e25f5d4d55bb7d18
Daniel <daniel.kaska2004@gmail.com>
2024-02-11 19:01:39 +0100

    created PostComment class for ef

---
+++ Forum/DB/Entities/PostComment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Forum.DB.Entities
+{
+    public class PostComment
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string Content { get; set; }
+        
+        public virtual Post Post { get; set; }
+        public int PostId { get; set; }
+        
+
+        public DateTime CommentCreatedDate { get; set; }
+
+    }
+}
